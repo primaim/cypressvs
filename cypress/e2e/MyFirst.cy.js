d4142177a98242009315158924f8b542977ff894
Simplylearn github <primaisraelmary24@gmail.com>
2023-05-05 09:23:31 +0530

    first commit

---
+++ cypress/e2e/MyFirst.cy.js
@@ -0,0 +1,14 @@
+
+describe('My First Test', () => {
+    it('verify title-positive', () => {
+       cy.visit("https://opensource-demo.orangehrmlive.com/") // To launch the URL
+       cy.title().should('eq','OrangeHRM')// To verify title of the Page
+  })
+
+  it('verify title-nagetive', () => {
+    cy.visit("https://opensource-demo.orangehrmlive.com/") // To launch the URL
+    cy.title().should('eq','OrangeHRM123')// To verify title of the Page
+})
+
+})
+
