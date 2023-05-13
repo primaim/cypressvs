8ea0305deaaa4adb3103e2d39c3d00b32104431b
Simplylearn github <primaisraelmary24@gmail.com>
2023-05-13 17:27:36 +0530

    Commiting all the files

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  chromeWebSecurity: false,
+  //chromeWebSecurity: false,
+  reporter: 'cypress-mochawesome-reporter', //to generate HTML reports
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on); // for html reports
 
     },
   },
