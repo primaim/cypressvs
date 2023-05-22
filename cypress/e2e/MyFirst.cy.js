aaf32aff6021a126545ac5309bb17efb3abce1d0
primaim <132450964+primaim@users.noreply.github.com>
2023-05-22 10:21:06 +0530

    Update MyFirst.cy.js

--- cypress/e2e/MyFirst.cy.js
+++ cypress/e2e/MyFirst.cy.js
@@ -4,9 +4,7 @@ describe('My First Test', () => {
        cy.visit("https://opensource-demo.orangehrmlive.com/") // To launch the URL
        cy.title().should('eq','OrangeHRM')// To verify title of the Page
   })
-    
-    
-   
+       
 })
 
     
